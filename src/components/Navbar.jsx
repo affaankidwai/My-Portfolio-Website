7038d956c160c161e4ba120f825c1e7ee99dde1a
Affaan Kidwai <kidwaiaffaan@gmail.com>
2022-09-10 22:57:54 +0530

    Added Hovering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,27 +18,27 @@ const Navbar = () => {
         {/* //menu */}
         <div>
         <ul className='hidden md:flex'>
-        <li>
+        <li className='text-white font-bold group px-6 py-4 my-2 flex items-center hover:bg-blue-700'>
           <Link to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li>
+        <li className='text-white font-bold group px-6 py-4 my-2 flex items-center hover:bg-blue-700'>
           <Link to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li>
+        <li className='text-white font-bold group px-6 py-4 my-2 flex items-center hover:bg-blue-700'>
           <Link to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li>
-        <li>
+        <li className='text-white font-bold group px-6 py-4 my-2 flex items-center hover:bg-blue-700'>
           <Link to='work' smooth={true} duration={500}>
             Work
           </Link>
         </li>
-        <li>
+        <li className='text-white font-bold group px-6 py-4 my-2 flex items-center hover:bg-blue-700'>
           <Link to='contact' smooth={true} duration={500}>
             Contact
           </Link>
